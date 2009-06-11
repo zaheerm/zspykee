546417698f67d8eb17d6e28ba6d3389d9da1b296
Zaheer Abbas Merali <zaheerabbas@merali.org>
2009-06-11 09:40:55 +0100

    Add correct license and copyright holder.

--- flumotion-spykee/flumotion/component/spykee/twistedprotocol.py
+++ flumotion-spykee/flumotion/component/spykee/twistedprotocol.py
@@ -1,4 +1,17 @@
-#!/usr/bin/python
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion Spykee component
+# Copyright (C) 2009 Zaheer Abbas Merali. All rights reserved.
+
+# This file may be distributed and/or modified under the terms of
+# the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+# This file is distributed without any warranty; without even the implied
+# warranty of merchantability or fitness for a particular purpose.
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
 
 from twisted.internet import protocol, reactor
 
